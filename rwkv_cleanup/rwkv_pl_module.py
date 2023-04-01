ed3b2f7493880dc594eeaab013e00086ed4bbb6b
Maximilian Beck <maximilianbeck@live.de>
2023-04-01 14:20:05 +0200

    add l2wrap from origrepo

--- rwkv_cleanup/rwkv_pl_module.py
+++ rwkv_cleanup/rwkv_pl_module.py
@@ -12,6 +12,21 @@ class RWKVModuleConfig:
     """Configuration for RWKVModule."""
     model: RWKVConfig
 
+class L2Wrap(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, loss, y):
+        ctx.save_for_backward(y)
+        return loss
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        y = ctx.saved_tensors[0]
+        # to encourage the logits to be close to 0
+        factor = 1e-4 / (y.shape[0] * y.shape[1])
+        maxx, ids = torch.max(y, -1, keepdim=True)
+        gy = torch.zeros_like(y)
+        gy.scatter_(-1, ids, maxx * factor)
+        return (grad_output, gy)
 
 class RWKVModel(pl.LightningModule):
 
@@ -37,9 +52,9 @@ class RWKVModel(pl.LightningModule):
         loss = self.loss(y_hat.view(-1, y_hat.size(-1)), y.view(-1))
         self.log("train_loss", loss)
         # TODO what does the masking stuff do?
-        # TODO use Loss wrapper
+        # TODO make loss wrapper configurable
 
-        return loss
+        return L2Wrap.apply(loss, y_hat)
 
     def configure_optimizers(self):
         betas = self.rwkv_args.get('betas', (0.9, 0.99))
