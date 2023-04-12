c4976ef40a06235d6109c3180ad29696364dda75
Maximilian Beck <maximilianbeck@live.de>
2023-04-12 10:39:17 +0200

    add ml_utilities template

---
+++ rwkv_dev/run_sweep.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from ml_utilities.run_utils.runner import run_sweep
+from ml_utilities.utils import get_config, get_config_file_from_cli
+from omegaconf import DictConfig
+
+
+def run(cfg: DictConfig):
+    run_sweep(cfg)
+
+if __name__=='__main__':
+    cfg_file = get_config_file_from_cli(config_folder='configs', script_file=Path(__file__))
+    cfg = get_config(cfg_file)
+    run(cfg)
